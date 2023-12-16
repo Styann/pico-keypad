0514991aa744afd65db685a288432da5408a5dfa
Styann <steunouyann44@gmail.com>
2023-12-16 13:50:07 +0100

    delete all files in /src/build (for gitignore)

--- src/build/CMakeFiles/pico-keypad.dir/Pico/pico-sdk/lib/tinyusb/src/class/net/ecm_rndis_device.c.obj.d
+++
@@ -1,6 +0,0 @@
-CMakeFiles/pico-keypad.dir/Pico/pico-sdk/lib/tinyusb/src/class/net/ecm_rndis_device.c.obj: \
- /Pico/pico-sdk/lib/tinyusb/src/class/net/ecm_rndis_device.c \
- /Pico/pico-sdk/lib/tinyusb/src/tusb_option.h \
- /Pico/pico-sdk/lib/tinyusb/src/common/tusb_compiler.h \
- /Pico/pico-sdk/lib/tinyusb/src/tusb_config.h \
- /Pico/pico-sdk/lib/tinyusb/src/common/tusb_mcu.h
--- src/build/CMakeFiles/pico-keypad.dir/Pico/pico-sdk/src/rp2_common/pico_stdio_usb/stdio_usb_descriptors.c.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/pico-keypad.dir/Pico/pico-sdk/src/rp2_common/pico_stdio_usb/stdio_usb_descriptors.c.obj: \
- /Pico/pico-sdk/src/rp2_common/pico_stdio_usb/stdio_usb_descriptors.c
